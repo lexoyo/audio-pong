642c6f8d5c15a50819da3baa832b1cc4cbb2cfc4
Alex Hoyau <a.hoyau@silexlabs.org>
2016-07-14 18:24:07 +0200

    ping... and a pong

---
+++ index.js
@@ -0,0 +1,50 @@
+var audio = require('./audio.json')
+console.log('STARTING', audio.length + ' audio')
+
+function getItems(dictionnary, min, max) {
+  var num = Math.ceil(min + Math.random() * (max - min));
+  var words = [];
+  var clone = JSON.parse(JSON.stringify(dictionnary));
+  for(i=0; i<num; i++) {
+    var idx = Math.floor(Math.random() * clone.length);
+    words = words.concat(clone.splice(idx, 1));
+  }
+  return words;
+}
+
+function playLocal(path, delay) {
+  setTimeout(function() {
+    var player = require('play-sound')(opts = {})
+    player.play(path, function(err) {
+      console.log('Done.', err)
+    })
+  }, delay);
+}
+
+
+
+/////////////////////////////////////////////////////////////////////
+
+
+var express = require('express');
+var app = express();
+
+app.use('/', express.static('./'));
+
+app.get('/ping', function (req, res, next) {
+
+  var chosen = getItems(audio, 1, 1)[0];
+
+  res.sendFile(__dirname + '/' + chosen.ping, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', chosen.ping, chosen.pong);
+      playLocal(__dirname + '/' + chosen.pong, chosen.delay);
+    }
+  });
+
+});
+app.listen(process.env.PORT || 8080);
